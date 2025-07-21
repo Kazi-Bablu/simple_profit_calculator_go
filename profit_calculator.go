32bfe12a4633001f7794055d7567498db17adaf5
Kazi-Bablu <kazibablubif@gmail.com>
2025-07-21 19:59:53 +0600

    code reformat

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,26 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var texRate float64
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tex Rate: ")
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tex Rate: ")
-	fmt.Scan(&texRate)
-
-	ebt := revenue - float64(expenses)
-	profit := float64(ebt) * (1 - texRate/100)
-
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+}
 
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - float64(expenses)
+	profit := float64(ebt) * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
 }
